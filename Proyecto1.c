fba47074bda89b578cd83d89296cffa6d6d3b76f
Niprois <alvarof628@gmail.com>
2023-10-04 21:42:19 -0300

    Update Proyecto1.c

--- Proyecto1.c
+++ Proyecto1.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct Jugador{
+char Nombre[50];
+int Vida;
+};
+
+struct Carta{
+char Nombre[50];
+int Ataque;
+int Vida;
+int Defensa;
+};
+
+struct Historial{
+
+};
+
 int main()
 {
 
