2ff66603d07107f6a9c458f240431d7869cde245
Niprois <alvarof628@gmail.com>
2023-10-15 11:02:17 -0300

    Comentarios añanidos

--- Proyecto1.c
+++ Proyecto1.c
@@ -22,43 +22,48 @@ struct Historial{
 
 int main()
 {
-    int Select;
-    int Retry = 1;
-    while (Retry == 1){
-        printf("Welcome to The clash of the guardians!\n What would like to do?\n 1) Play.\n 2) Create a new card.\n 3) See the battle record.\n 4) Exit the game.");
-        scanf("%d" ,&Select);
-        if(Select == 1){
-            printf("Entering the game...\n");
-            Retry = 0;
-        }
-        if(Select == 2){
-            printf("Entering the card creator...\n");
-
-        }
-        if(Select == 3){
-            printf("Now showing past outcomes...\n");
-
-        }
-        if(Select == 4){
-            printf("Goodbye, Player!\n");
-            exit(0);
-        }
-        else{
-            printf("That's not a valid option!");
-        }
-
-    }
-    FILE *file = fopen("Cartas.txt", "r");
-    if (file == NULL) {
+    int Select; //Permite seleccionar una opcion al inicio.
+    int Retry = 1; //Si esta activa, se repite el menu.
+    FILE *file = fopen("Cartas.txt", "r");//Abrir archivo.
+    if (file == NULL) { //Si el archivo no esta.
         perror("Error opening file");
         return 1;
     }
+    while (Retry == 1){//Menu de inicio
+        printf("Welcome to The clash of the guardians!\n What would like to do?\n 1) Play.\n 2) Create a new card.\n 3) See the battle record.\n 4) Exit the game.\n");
+        scanf("%d" ,&Select);
+        
+        if(Select == 1){ //Empezar juego.
+            printf("Entering the game...\n");
+            Retry = 0;
+            break;
+        }
+        if(Select == 2){ //Crear una carta
+            printf("Entering the card creator...\n");
+            
+            continue;
+        }
+        if(Select == 3){ //Mostrar historial
+            printf("Now showing past outcomes...\n");
 
-    struct Carta cartas[100];
+            continue;
+        }
+        if(Select == 4){ //Salir del juego
+            printf("Goodbye, Player!\n");
+            exit(0);
+        }
+        else{ //Si se introduce cualquier otro valor
+            printf("That's not a valid option!");
+            continue;
+        }
 
-    int NumeroCartas = 0;
+    }
 
-    while (fscanf(file, "%s %s %d %d %d", cartas[NumeroCartas].Name,
+    struct Carta cartas[100]; //Creación de struct
+
+    int NumeroCartas = 0; //Contador de cartas
+
+    while (fscanf(file, "%s %s %d %d %d", cartas[NumeroCartas].Name, //Leer cartas desde archivo
     cartas[NumeroCartas].Type,&cartas[NumeroCartas].AP,&cartas[NumeroCartas].LP,&cartas[NumeroCartas].DP) == 5) {
 
         NumeroCartas++;
@@ -68,10 +73,16 @@ int main()
             break;
         }
     }
+    
 
     fclose(file);
 
-    printf("List of cards\n");
+    //Sistema de pelea
+
+
+
+
+    printf("List of cards\n");//Imprimir las cartas existentes
     for (int i = 0; i < NumeroCartas; i++) {
         printf("Name: %s, Type: %s, Life: %d, Attack: %d, Defense: %d\n", cartas[i].Name,
         cartas[i].Type, cartas[i].LP, cartas[i].AP, cartas[i].DP);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Proyecto1
+Creado por Alvaro Fuentes
+Fecha de entrega: 15/10/2023
+Carrera: IDVRV
+Clase y sección: 
